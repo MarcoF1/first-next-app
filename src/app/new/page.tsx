68606c551c65dd91c1dd0bab0c4197efadcc18f8
MarcoF1 <marco1fleming@gmail.com>
2023-07-21 11:16:24 -0400

    checkboxes and new page with form

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <h1>new route</h1>;
+export default function Page() {
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">New</h1>
+      </header>
+      <form>
+        <input type="text" name="title"></input>
+      </form>
+    </>
+  );
 }
