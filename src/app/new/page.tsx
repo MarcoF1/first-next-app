c50f76253cf99cfbc83162d042c7a388a8291a28
MarcoF1 <marco1fleming@gmail.com>
2023-07-21 11:17:21 -0400

    style input form

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -5,7 +5,11 @@ export default function Page() {
         <h1 className="text-2xl">New</h1>
       </header>
       <form>
-        <input type="text" name="title"></input>
+        <input
+          type="text"
+          name="title"
+          className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"
+        ></input>
       </form>
     </>
   );
